3a735a0738a21a106dd48dd0b9014e535cd3febe
Julius Bacosa <julius@innoventsoftware.com.au>
2019-04-07 09:26:14 +0800

    singleton

---
+++ SingletonErrorManager.ErrorManager/ErrorSingleton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonErrorManager.ErrorManager
+{
+    public sealed class ErrorSingleton
+    {
+        public List<string> Errors { get; set; }
+
+        private static volatile ErrorSingleton _instance;
+        private static readonly object _syncLock = new object();
+        private ErrorSingleton()
+        {
+            Errors = new List<string>();
+        }
+
+        public static ErrorSingleton Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (_syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ErrorSingleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+
+        public async Task<List<string>> GetSomething(string keyword)
+        {
+            if(keyword != null)
+            {
+                return Errors.Where(x => x.Contains(keyword))?.ToList();
+            }
+            else
+            {
+                return Errors;
+            }
+            
+        }
+
+        public async Task AddSomething(string error)
+        {
+            Errors.Add(error);
+        }
+    }
+}
---
+++ SingletonErrorManager.WebApi/Controllers/Test1Controller.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SingletonErrorManager.ErrorManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingletonErrorManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test1Controller : ControllerBase
+    {
+        public Test1Controller()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("get1/{keyword}")]
+        public async Task<IActionResult> Get([FromRoute] string keyword)
+        {
+            return Ok(await ErrorSingleton.Instance.GetSomething(keyword));
+        }
+
+        [HttpGet]
+        [Route("get2")]
+        public async Task<IActionResult> Get2()
+        {
+            return Ok(await ErrorSingleton.Instance.GetSomething(null));
+        }
+
+        [HttpGet]
+        [Route("add/{keyword}")]
+        public async Task<IActionResult> Add([FromRoute] string keyword)
+        {
+            await ErrorSingleton.Instance.AddSomething(keyword);
+            return Ok();
+        }
+    }
+}
